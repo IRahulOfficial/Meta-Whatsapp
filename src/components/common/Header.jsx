0801f7f6b0d085a7b9c1137acb22154fed653170
vivek kumar <vivekkumarvk1707@gmail.com>
2024-05-18 10:10:40 +0530

    changes

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -27,7 +27,7 @@ function Header() {
               </svg>
             </div>
             <ul
-              tabIndex={0}
+              // tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
@@ -86,7 +86,6 @@ function Header() {
           </div>
         </div>
       </div>
-      
     </>
   );
 }
---
+++ src/components/common/SideBar copy.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <>
+    <div className="drawer drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+      
+        
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu p-4 w-72 min-h-full bg-base-200 text-base-content">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default SideBar
